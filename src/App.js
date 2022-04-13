38d05afecccfd184abc90c024823351f34f00105
ShayFae <ifrahnoor108@gmail.com>
2022-04-13 00:50:23 -0400

    Add components

--- src/App.js
+++ src/App.js
@@ -1,11 +1,27 @@
 import './App.css';
-import Timer from './component/Timer';
+import Twenty from './component/Twenty';
+import Ten from './component/Ten';
+import Five from './component/Five';
+
+import { useState } from 'react';
 
 function App() {
+  const [find, setFind] = useState('one');
+
   return (
     <div className="App">
         <h1>Pomodoro</h1>
-        <Timer />
+        <button onClick={() => setFind('one')}>Twenty Five Minutes</button>
+        <button onClick={() => setFind('two')}>Ten Minutes</button>
+        <button onClick={() => setFind('three')}>Five Minutes</button>
+
+        {find === 'one' && <Twenty />}
+        {find === 'two' && <Ten />}
+        {find === 'three' && <Five />}
+
+
+        {/* {find ? <Timer /> : <Meep />} */}
+        {/* <Meep /> */}
     </div>
   );
 }
---
+++ src/component/Ten.js
@@ -0,0 +1,27 @@
+import './Timer.css'
+import { useEffect, useState } from 'react'
+
+export default function Meep() {
+  const [start, setStart] = useState(false);
+  const [num, setNum] = useState(600)
+
+  //TIMER
+    useEffect(() =>  {
+      if (start) {
+        if(num > 0) {
+          setTimeout(() => setNum(prev => --prev), 1000)
+        } else {
+          return () => clearTimeout(num);
+        }
+      }
+    }, )
+
+  return (
+    <div className="timer">
+      <h1>Set Timer</h1>
+                <h3>{num} </h3> 
+                <button onClick={() => setStart(true)}>START</button>
+                <button onClick={() => setStart(false)}>STOP</button>
+    </div>
+  );
+}
